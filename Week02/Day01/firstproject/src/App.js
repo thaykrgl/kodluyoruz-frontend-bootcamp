88cbf1134dbc3ad1a2f591327afe1582fad03758
Taha Aykıroğlu <tahaaykiroglu99@gmail.com>
2022-07-26 17:32:29 +0300

    Updated App.js

--- Week02/Day01/firstproject/src/App.js
+++ Week02/Day01/firstproject/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Paragraph from './Paragraph';
 import Title from './Title';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <Paragraph />
     </div>
   );
 }
